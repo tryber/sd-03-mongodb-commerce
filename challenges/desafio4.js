f94c4eae1dd75a9a89f840d524fd7f053b7c1719
William Igor <williamigorsilva@gmail.com>
2020-08-06 17:53:10 -0300

    expected tabs but not found

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,10 +1,10 @@
 db.produtos.updateOne({nome: "Big Mac"}, {
   $currentDate: { 
       ultimaModificacao: {
-         $type: "date"
+          $type: "date"
         } 
       }
-   }
- );
+    }
+  );
 
 db.produtos.find({ultimaModificacao: {$exists:  true} }, {nome: 1, _id:0});
