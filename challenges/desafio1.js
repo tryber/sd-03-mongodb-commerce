e0e2715cdedd5313c244ad4485ee89267770ab22
Marco Barbosa <marco.meireles.b@gmail.com>
2020-08-06 19:30:31 -0300

    desafio3

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,2 +1,2 @@
-db.produtos.updateMany({},{$set: {"criadoPor": "Ronald McDonald"}});
+db.produtos.updateMany({"criadoPor": {$exists: 0}},{$set: {"criadoPor": "Ronald McDonald"}}, {multi: true});
 db.produtos.find({},{"_id": 0,"nome": 1, "criadoPor":1}).pretty();
