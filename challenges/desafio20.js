22391a1ac9e4c658ca7f0fcc0804cab81f3660ce
Gustaff <gustavohmfigueiredo@gmail.com>
2020-08-06 18:27:50 -0300

    aspas duplas na 5

--- challenges/desafio20.js
+++ challenges/desafio20.js
@@ -1,6 +1,6 @@
 db.produtos.updateOne(
   { nome:  "Big Mac" },
-  { $unset: { curtidas: '' } }
+  { $unset: { curtidas: "" } }
 );
 
 db.produtos.find({}, { _id: 0, curtidas: 1, nome: 1 });
--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -10,7 +10,4 @@ db.produtos.updateMany(
     }
   }
 );
-db.produtos.find(
-  {},
-  { _id: 0, nome: 1, ingredientes: 1 }
-);
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
