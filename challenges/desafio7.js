2f40e599375c82588c6b1533952db0dfc8e52669
Lucas Werner <lucas.1werner@gmail.com>
2020-08-06 18:21:23 -0300

    partiu cc

---
+++ challenges/desafio7.js
@@ -0,0 +1,14 @@
+db.produtos.updateMany({},
+  {
+    $pull: {
+      "ingredientes": "cebola"
+    }
+  }
+  );
+
+  db.produtos.find({},
+    {
+      "_id": 0,
+      "nome": 1,
+      "ingredientes": 1,
+    });
