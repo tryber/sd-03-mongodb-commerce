52d4e37945755d60396638a67b2e6e218ec7e078
mirellaalves <mirella.alves@gmail.com>
2020-08-06 16:32:31 -0300

    desafio3

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -5,3 +5,23 @@
 // Crie uma query que incremente o valor do campo avaliacao em 5 em todos os sanduíches de carne do tipo bovino. Dica: utilize como filtro o campo tags.
 // Crie uma query que incremente o valor do campo avaliacao em 3 em todos os sanduíches de ave.
 // Crie uma query que retorne o nome e avaliacao de todos os sanduíches.
+
+db.produtos.updateMany(
+  {},
+  {$set: {avaliacao: NumberInt("0")}}
+);
+
+db.produtos.updateMany(
+  {tags: "bovino"},
+  {$inc: {avaliacao: 5}}
+);
+
+db.produtos.updateMany(
+  {tags: "ave"},
+  {$inc: {avaliacao: 3}}
+);
+
+db.produtos.find(
+  {},
+  { _id: 0, nome: 1, avaliacao: 1}
+);
