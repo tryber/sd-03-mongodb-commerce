2b5ea23772a0d771a7aacd54927e6d6140a9fdc3
Gustaff <gustavohmfigueiredo@gmail.com>
2020-08-06 18:12:52 -0300

    9.js projection with bool and numbers

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -2,4 +2,4 @@ db.produtos.update(
   { nome: "Cheddar McMelt" },
   { $pop: { ingredientes: 1 } }
 );
-db.produtos.find({}, { _id: null, nome: 'a', ingredientes: 'ok' });
+db.produtos.find({}, { _id: null, nome: true, ingredientes: 1 });
