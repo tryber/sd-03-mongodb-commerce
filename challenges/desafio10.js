0936dc8f8f83c968cf4badb37bdf3365ef13172a
fabemiliano <fab.emiliano@gmail.com>
2020-08-06 18:20:30 -0300

    first commit

---
+++ challenges/desafio10.js
@@ -0,0 +1,4 @@
+db.produtos.updateMany({}, { $push: { vendasPorDia: { $each: [0, 0, 0, 0, 0, 0, 0] } } });
+db.produtos.updateMany({ nome: "Big Mac" }, { $inc: { "vendasPorDia.3": 60 } });
+db.produtos.updateMany({ tags: "bovino" }, { $inc: { "vendasPorDia.6": 120 } });
+db.produtos.find({}, { nome: 1, vendasPorDia: 1, _id: 0 });
