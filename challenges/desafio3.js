8fcabe56d2f8d5baa6112001061decb3b198e15d
Marco Barbosa <marco.meireles.b@gmail.com>
2020-08-06 19:35:10 -0300

    codeclimate

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,4 +1,4 @@
-db.produtos.updateMany({"avaliacao": {$exists: 0}},{$set: {"avaliacao": NumberInt("0")}},{multi: true});
+db.produtos.updateMany({"avaliacao": {$exists: false}},{$set: {"avaliacao": NumberInt("0")}},{multi: true});
 db.produtos.updateMany({"tags":{$all: ["bovino"]}},{$inc: {"avaliacao": 5}});
 db.produtos.updateMany({"tags":{$all: ["ave"]}},{$inc: {"avaliacao": 3}});
 db.produtos.find({},{"_id": 0,"nome": 1, "avaliacao":1}).pretty();
