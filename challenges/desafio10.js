a190eb0457d4257d2a3536430e1034acf4844f30
Gustaff <gustavohmfigueiredo@gmail.com>
2020-08-06 16:14:03 -0300

    10.js complete and passing

---
+++ challenges/desafio10.js
@@ -0,0 +1,10 @@
+db.produtos.updateMany(
+  {},
+  { $push: { vendasPorDia: { $each: [0, 0, 0, 0, 0, 0, 0] } } },
+);
+db.produtos.update(
+  { nome: "Big Mac" },
+  { $set: { "vendasPorDia.3": 60 } }
+);
+db.produtos.update({ tags: { $all: ["bovino", "pão"] } }, { $set: { "vendasPorDia.6": 120 } });
+db.produtos.find({}, { _id: 0, nome: 'nome', vendasPorDia: 'vendasPorDia' });
