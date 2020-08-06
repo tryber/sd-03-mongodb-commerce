d0ae1cd0735ff3faa9b24eb51ac8fa6ccad79289
Gustaff <gustavohmfigueiredo@gmail.com>
2020-08-06 18:09:21 -0300

    3.js update to updateMany

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -6,7 +6,7 @@ db.produtos.updateMany(
   { tags: { $all: ['bovino'] } },
   { $inc: { avaliacao: 5 } },
 );
-db.produtos.update(
+db.produtos.updateMany(
   { tags: { $all: ['ave'] } },
   { $inc: { avaliacao: 3 } },
   { multi: true }
