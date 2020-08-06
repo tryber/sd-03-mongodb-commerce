dfc2e38b7faf4ec6a8ea2eb002ff5345ddfbb17a
William Igor <williamigorsilva@gmail.com>
2020-08-06 18:05:57 -0300

    alter 13 and 14

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,13 +1,19 @@
-// db.produtos.updateMany({"valoresNutricionais.tipo":"sódio",
-//     "valoresNutricionais.percentual": {$gte: 40}},{
-//         $push: { "tags": "muito sódio"}
-//     }
-// );
-
-// db.produtos.find({}, {nome: 1, tags:1, _id: 0});
-
-db.produtos.updateMany({"nome": "Big Mac"},
-{ $push: { "tags": "muito sódio"}}
+db.produtos.updateMany({
+  "valoresNutricionais": {
+    $elemMatch: {
+      tipo: "sódio",
+      percentual: { $gte: 40}
+    }
+  }
+},{
+  $push: { "tags": "muito sódio" }
+}
 );
 
 db.produtos.find({}, {nome: 1, tags:1, _id: 0});
+
+// db.produtos.updateMany({ "nome": "Big Mac" },
+//   { $push: { "tags": "muito sódio" } }
+// );
+
+// db.produtos.find({}, { nome: 1, tags: 1, _id: 0 });
