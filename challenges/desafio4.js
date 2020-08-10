81d22f9a6a9174f04ef859d127dcd4cbe682e5c1
Willy Catão <50681413+wscatao@users.noreply.github.com>
2020-08-10 14:51:37 -0300

    projeto commerce

---
+++ challenges/desafio4.js
@@ -0,0 +1,17 @@
+db.produtos.updateMany({
+  nome: "Big Mac"
+}, {
+  $currentDate: {
+      ultimaModificacao: {
+          $type: "date"
+      }
+  }
+});
+db.produtos.find({
+  ultimaModificacao: {
+      $exists: true
+  }
+}, {
+  _id: 0,
+  nome: 1
+});
