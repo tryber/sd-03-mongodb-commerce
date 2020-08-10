8c8ae8934ef64d10f6da004dccb7b9e966398827
Gustavo <ggteodor07@gmai.com>
2020-08-10 17:14:48 -0300

    iniciando o projeto MongoDB Commerce

---
+++ challenges/desafio3.js
@@ -0,0 +1,29 @@
+db.produtos.updateMany(
+  { "avaliacao":
+    { $exists: false },
+  },
+  { $set:
+    { "avaliacao": NumberInt("0") },
+  },
+);
+
+db.produtos.updateMany(
+  { "tags":
+    { $all: ["bovino"] },
+  },
+  { $inc:
+    { "avaliacao": 5 },
+  },
+);
+
+db.produtos.updateMany(
+  { "tags":
+    { $all: ["ave"] },
+  },
+  { $inc:
+    { "avaliacao": 3 },
+  },
+);
+
+db.produtos.find({},{ "nome":1, "avaliacao":1, "_id":0 }).pretty();
+  
