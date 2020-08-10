f3708a1dc5ce0855d355a97c7b9806df33c81cd8
Rhian Castro <rhi.castro@gmail.com>
2020-08-09 21:04:24 -0300

    cc fix

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,4 +1,3 @@
- 
 db.produtos.updateMany({}, { $set : { "avaliacao": NumberInt("0") } });
 db.produtos.updateMany({ "tags": "bovino" }, { $inc: { "avaliacao": 5 } });
 db.produtos.updateMany({ "tags": "ave"}, { $inc: { "avaliacao": 3 } });
