cdf173358b795fa01c1facc82cf5061bacdb792a
Henrique Rezende <henriquerezendec@gmail.com>
2020-08-10 21:49:49 -0300

    Desafio 16

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,5 +1,5 @@
 db.produtos.updateMany(
-  { valoresNutricionais: { $elemMatch: { "tipo": "sódio" , percentual: { $gte: 40} } } },
+  { "valoresNutricionais": { $elemMatch: { "tipo": "sódio" , "percentual": { $gte: 40} } } },
   { $push: { "tags": "muito sódio" } }
 );
 
--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,3 +1,3 @@
-db.produtos.updateMany({ $or: [{ nome: "Big Mac" }, { nome: "Quarteirão com Queijo" }]},{ $addToSet: { "ingredientes": "bacon" } });
+db.produtos.updateMany({ $or: [{ "nome": "Big Mac" }, { "nome": "Quarteirão com Queijo" }]},{ $addToSet: { "ingredientes": "bacon" } });
 
 db.produtos.find({}, {"_id": 0, "nome": 1, "ingredientes": 1});
