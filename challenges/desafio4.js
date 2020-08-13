73534a91ffe662758f70360b1f2065e912ab3bca
renandreolle@hotmail.com <renandreolle@hotmail.com>
2020-08-12 22:26:05 -0300

    4

---
+++ challenges/desafio4.js
@@ -0,0 +1,16 @@
+db.produtos.update(
+    {"nome":"Big Mac"},
+    {
+      $set: {
+        "ultimaModificacao": new Date()    
+      }
+    }
+);
+
+db.produtos.find(
+    {"ultimaModificacao": {$exists: true}},
+    {
+      "nome": 1,
+      "_id": 0    
+    }
+);
