e0ebb826cc1f9ad4858fb7226c17c00fb56e29fb
renandreolle@hotmail.com <renandreolle@hotmail.com>
2020-08-13 11:32:03 -0300

    agora vai

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,8 +1,8 @@
 db.produtos.updateMany(
     {},
     { 
-      $push: { "tags": {$each: ["combo","tasty"]},
-      $sort: {"tags": 1}}
+      $push: { "tags": {$each: ["combo","tasty"],
+      $sort: {"tags": 1}}}
     }
 );
 
